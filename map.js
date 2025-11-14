676bab0b25695d8ced028e63e948afd23082476b
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-11-14 00:34:44 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3lsZWxlMzIyMSIsImEiOiJjbWh5a2tiN3AwZGEyMmtvbWt1M3NmbWpwIn0.jzjnBS1_Hf0fLheOMe33PA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18
+});
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
