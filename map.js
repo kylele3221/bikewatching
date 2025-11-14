478742e45c9618600154961525b3e927c55eabbb
Kyle Le <113484711+kylele3221@users.noreply.github.com>
2025-11-14 00:40:42 -0800

    Update map.js

--- map.js
+++ map.js
@@ -12,3 +12,21 @@ const map = new mapboxgl.Map({
 });
 
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+map.on('load', async () => {
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4
+    }
+  });
+});
